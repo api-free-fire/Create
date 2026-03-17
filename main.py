b26b7c3cb8d76b1c3d2e430228b3e4ad32d99869
ARY4N09 <tlaryan74@gmail.com>
2026-03-17 07:05:10 +0545

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ try:
 except:
     print('\033[0;1m[\033[1;33m!\033[0;1m]\033[0;1m NO GIT REPOSITORY FOUND')
 if bit == '64bit':
-    import main
+    import main64
 elif bit == '32bit':
     print('\033[0;1m[\033[1;31m✗\033[0;1m]\033[0;1m 32BIT NOT SUPPORTED (only 64bit .so file available)')
 else:
